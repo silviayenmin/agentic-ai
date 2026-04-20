6f6e714baba70d4db55e1a4dc338f1624ef82736
silvia-yenmin <silvia.yenmin@gmail.com>
2026-04-20 19:22:31 +0530

    Revert "init changes"
    This reverts commit b62153381dfcdaf7a1dbe0966820e0b1e50bb182.

--- backend/Tools/__init__.py
+++ backend/Tools/__init__.py
@@ -1,4 +1,3 @@
-from .create_file import create_file_if_not_exists
 from .file_permissions import check_file_permissions
 from .os_permission import request_os_permission
 from .process_manager import execute_command, stop_process
@@ -7,6 +6,5 @@ __all__ = [
     "check_file_permissions", 
     "request_os_permission", 
     "execute_command", 
-    "stop_process",
-    "create_file_if_not_exists"
+    "stop_process"
 ]
