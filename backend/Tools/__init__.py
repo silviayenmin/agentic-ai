b62153381dfcdaf7a1dbe0966820e0b1e50bb182
silvia-yenmin <silvia.yenmin@gmail.com>
2026-04-20 19:20:12 +0530

    init changes

--- backend/Tools/__init__.py
+++ backend/Tools/__init__.py
@@ -1,3 +1,4 @@
+from .create_file import create_file_if_not_exists
 from .file_permissions import check_file_permissions
 from .os_permission import request_os_permission
 from .process_manager import execute_command, stop_process
@@ -6,5 +7,6 @@ __all__ = [
     "check_file_permissions", 
     "request_os_permission", 
     "execute_command", 
-    "stop_process"
+    "stop_process",
+    "create_file_if_not_exists"
 ]
--- backend/tools/__init__.py
+++ backend/tools/__init__.py
@@ -1,7 +1,12 @@
 from .create_file import create_file_if_not_exists
+from .file_permissions import check_file_permissions
+from .os_permission import request_os_permission
+from .process_manager import execute_command, stop_process
 
-__all__ = ["create_file_if_not_exists"]
-
-# This allows you to import everything like:
-# from tools import create_file_if_not_exists
-# Or: from tools import *
+__all__ = [
+    "check_file_permissions", 
+    "request_os_permission", 
+    "execute_command", 
+    "stop_process",
+    "create_file_if_not_exists"
+]
